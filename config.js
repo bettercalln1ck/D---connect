b2146af848f962c3e9e1a44aa9303a3f50c1571f
nikolai6303 <iit2018152@iiita.ac.in>
2020-11-11 00:57:47 +0530

    changed review

--- config.js
+++ config.js
@@ -1,5 +1,7 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
+    'GITHUB_CLIENT_SECRET':"78568bdf44f70016b9fc7b28e7a08b2f82badfab",
+    'GITHUB_CLIENT_ID':"cdce7dc3df7fce75f94d",
     'mongoUrl': 'mongodb://localhost:27017/conFusion',
     'facebook': {
         clientId: '3674195969278342',
