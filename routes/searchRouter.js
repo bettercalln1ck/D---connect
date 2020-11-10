40657a7468929017554bb6e3aedc152265e5811b
barney-stinson <iit2018081@iiita.ac.in>
2020-11-11 01:53:21 +0530

    added group search

--- routes/searchRouter.js
+++ routes/searchRouter.js
@@ -52,4 +52,25 @@ searchRouter.route('/post')
       });
 });
 
+searchRouter.route('/group')
+.post(cors.corsWithOptions,authenticate.verifyUser,(req, res, next)=> {
+    argument=req.body.argument;
+    console.log(argument);
+        const python = spawn("python", ["./routes/scripts/searchGroups.py", argument]);
+        python.stdout.on("data", (data) => {
+        console.log("data receiving from python script");
+        datatosend = data.toString();
+        console.log(`${datatosend}`);
+      });
+      python.on("close", (code) => {
+        console.log(`child process closes with code ${code}`);
+  
+      console.log(JSON.parse(datatosend));
+        user=JSON.parse(datatosend);
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json({success: true,user});
+      });
+});
+
 module.exports = searchRouter;
